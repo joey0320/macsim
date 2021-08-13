49a15d343bf7265801f2c5fa3ac2f53aa23ec1df
joey0320 <joonho0320@gmail.com>
2021-08-13 09:05:22 +0900

    fix run.sh
    --exclusive options doesn't seem to work as I thought
    should make job array instead

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,15 +6,19 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=joonho0320@gmail.com
 #
-#SBATCH --ntasks=8
+#SBATCH --ntasks=1
+#SBATCH --array=0-13
 
 IOMMU_EN_LIST=(0 1)
 PTW_CNT_LIST=(1 4 8 16 32 64 128)
 
-for PTW_CNT in 1 4 8 16 32
-do
-  srun -N1 -n1 -c1 --exclusive mkdir --out=saxpy-${PTW_CNT}
-  srun -N1 -n1 -c1 --exclusive ./macsim --out=saxpy-${PTW_CNT} --ptw_cnt=${PTW_CNT} --pts_entry=${PTW_CNT} --prmb_cnt=${PTW_CNT}
-done
-wait
+IOMMU_EN_ID=$((${SLURM_ARRAY_TASK_ID}%2))
+PTW_CNT_ID=$((${SLURM_ARRAY_TASK_ID}%7))
 
+IOMMU_EN=${IOMMU_EN_LIST[${IOMMU_EN_ID}]}
+PTW_CNT=${PTW_CNT_LIST[${PTW_CNT_ID}]}
+
+OUT_DIR="saxpy-${PTW_CNT}-${IOMMU_EN}"
+
+srun mkdir ${OUT_DIR}
+srun ./macsim --out=${OUT_DIR} --ptw_cnt=${PTW_CNT} --pts_entry=${PTW_CNT} --prmb_cnt=0
